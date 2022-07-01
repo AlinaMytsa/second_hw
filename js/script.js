9449f05e0bc0b0b355dbbb3b505c2d0d18d84507
AlinaMytsa <act.mitsa@gmail.com>
2022-07-01 21:44:31 +0300

    update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// task1
+let name = "John";
+let admin = name;
+alert(admin);
+
+// task2
+let object = "Earth";
+let user = "username";
+
+// task3
+let str = "10";
+let number = 10;
+
+console.log(typeof (str));
+
+console.log(typeof (number));
